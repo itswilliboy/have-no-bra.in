cfa1321ae289c341d3ff216d86e7fdda5273074b
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-03-17 22:56:35 +0100

    Add tailwind formatting

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex h-screen w-screen items-center justify-center bg-blue-950">
-    <h1 class="text-6xl text-bold text-white">Hello, World.</h1>
+    <h1 class="text-bold text-6xl text-white">Hello, World.</h1>
   </div>
 </template>
