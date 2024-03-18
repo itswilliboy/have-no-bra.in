e84be2b974520bae5db62ac56ad0288ad1e09e5e
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-03-18 21:54:23 +0100

    Edit configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
@@ -7,5 +10,11 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  modules: ["nuxt-icon", "@nuxtjs/color-mode"],
+  colorMode: {
+    classSuffix: "",
+    preference: "dark",
+    fallback: "dark",
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purpur-100": "#E0AAFF",
+        "purpur-200": "#C77DFF",
+        "purpur-300": "#9D4EDD",
+        "purpur-400": "#7B2CBF",
+        "purpur-500": "#5A189A",
+        "purpur-600": "#3C096C",
+        "purpur-700": "#240046",
+        "purpur-800": "#10002B",
+      },
+    },
+    fontFamily: {
+      onest: ["Onest"],
+    },
   },
   plugins: [],
+  darkMode: "class",
 }
