5e7d4375f01914d54bf951847502dda6327629a4
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-03-18 21:54:49 +0100

    Add pages and components

---
+++ components/ThemeButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-white">
+    <button
+      class="-m-4 flex flex-col items-center justify-center rounded-lg bg-white/20 p-3 text-xs transition-colors hover:bg-white/30"
+      @click="swapTheme()"
+    >
+      <Icon
+        name="solar:moon-fog-bold-duotone"
+        color="white"
+        size="24px"
+        v-if="['dark', 'system'].includes($colorMode.preference)"
+      />
+      <Icon name="solar:sun-fog-bold-duotone" color="white" size="24px" v-else />
+    </button>
+  </div>
+</template>
+
+<script setup>
+const swapTheme = () => {
+  if (["dark", "system"].includes(colour.preference)) {
+    colour.preference = "light"
+  } else {
+    colour.preference = "dark"
+  }
+}
+const colour = useColorMode()
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-grow justify-center">
+    <h1 class="mt-32 text-3xl font-semibold">Hello, World!</h1>
+  </div>
+</template>
