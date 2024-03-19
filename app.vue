c8030d1ec369dd076d1ca9d7a54c7761eee516a7
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-03-20 00:02:40 +0100

    Various changes

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="flex h-screen min-h-screen min-w-[100vw] flex-col font-onest text-black dark:bg-purpur-800 dark:text-white">
+  <div class="flex h-screen min-h-screen flex-col overflow-x-clip font-onest text-black dark:bg-purpur-800 dark:text-white">
     <NuxtLayout>
       <header class="shadow-2xl"><Navbar /></header>
-      <main class="flex flex-grow"><NuxtPage /></main>
+      <main id="main" class="flex flex-grow"><NuxtPage /></main>
+      <footer id="footer" class="z-10 min-h-24 w-screen bg-purpur-700"><Footer /></footer>
     </NuxtLayout>
   </div>
 </template>
@@ -10,3 +11,16 @@
 <style>
 @import "./assets/css/main.css";
 </style>
+
+<style scoped>
+#main {
+  min-height: calc(100vh - var(--header-height));
+}
+</style>
+
+<script setup lang="ts">
+useCookie("imageHidden", {
+  default: () => "false",
+  sameSite: "strict",
+})
+</script>
